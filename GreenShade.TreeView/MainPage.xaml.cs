7c8ea8c12ac0bc801d01577c22001227bd875359
zhangguangjian <zhangguangjian@huodongxing.com>
2019-10-17 18:51:22 +0800

    添加项目文件。

---
+++ GreenShade.TreeView/MainPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using GreenShade.TreeView.Models;
+using Windows.Storage;
+using System.Collections.ObjectModel;
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace GreenShade.TreeView
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ObservableCollection<Son> DataSource1 = new ObservableCollection<Son>();
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            base.OnNavigatedTo(e);
+            // StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            var sam1 = await storageFolder.TryGetItemAsync("Template.txt");
+            if (sam1 != null)
+            {
+                StorageFile templateFile1 = await storageFolder.GetFileAsync("Template.txt");
+                if (templateFile1 != null)
+                {
+                    string model = await Windows.Storage.FileIO.ReadTextAsync(templateFile1);
+                    if (!string.IsNullOrWhiteSpace(model))
+                    {
+                        ObservableCollection<Son> appsData = Newtonsoft.Json.JsonConvert.DeserializeObject<ObservableCollection<Son>>(model);
+                        foreach (var item in appsData)
+                        {
+                            DataSource1.Add(item);
+                        }
+                        TreeView1.ItemsSource = DataSource1;
+
+                    }
+                }
+            }
+            else
+            {
+                var localFolder = ApplicationData.Current.LocalFolder;
+
+                var originalDbFileUri = new Uri("ms-appx:///Assets/Template.txt");
+
+                var originalDbFile = await StorageFile.GetFileFromApplicationUriAsync(originalDbFileUri);
+                if (null != originalDbFile)
+                {
+                  await originalDbFile.CopyAsync(localFolder, "Template.txt", NameCollisionOption.ReplaceExisting);
+                    StorageFile templateFile1 = await storageFolder.GetFileAsync("Template.txt");
+                    string model = await Windows.Storage.FileIO.ReadTextAsync(templateFile1);
+                    if (!string.IsNullOrWhiteSpace(model))
+                    {
+                        ObservableCollection<Son> appsData = Newtonsoft.Json.JsonConvert.DeserializeObject<ObservableCollection<Son>>(model);
+                        foreach (var item in appsData)
+                        {
+                            DataSource1.Add(item);
+                        }
+                        TreeView1.ItemsSource = DataSource1;
+
+                    }
+                }
+            }
+        }
+    }
+
+    public class ExplorerItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FolderTemplate { get; set; }
+        public DataTemplate FolderTemplate1 { get; set; }
+        public DataTemplate FileTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            var explorerItem = (Son)item;
+            //var explorerItem = explorer.Content as Son;
+            if (!string.IsNullOrEmpty(explorerItem.list_icon))
+            {
+                return FolderTemplate;
+            }
+            else if (string.IsNullOrEmpty(explorerItem.list_icon) && explorerItem.sons != null && explorerItem.sons.Count > 0)
+            {
+                return FolderTemplate1;
+            }
+            else
+            {
+                return FileTemplate;
+            }
+            // return FolderTemplate;
+            // return explorerItem.sons!=null ? FolderTemplate : FileTemplate;
+        }
+    }
+}
---
+++ GreenShade.TreeView/Models/AllAppsData.cs
@@ -0,0 +1,123 @@
+﻿using GreenShade.TreeView.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GreenShade.TreeView.Models
+{
+    public class AllAppsData
+    {
+        public string stat { get; set; }
+        public string msg { get; set; }
+        public SonData data { get; set; }
+
+
+    }
+    public class SonData
+    {
+        public Info info { get; set; }
+        public ObservableCollection<Son> datas { get; set; }
+        public object[] promotions { get; set; }
+    }
+
+    public class Info
+    {
+        public string update_date { get; set; }
+    }
+
+    public class Son : INotifyPropertyChanged
+    {
+        private ICommand _switchThemeCommand;
+
+        public ICommand SwitchThemeCommand
+        {
+            get
+            {
+                if (_switchThemeCommand == null)
+                {
+                    _switchThemeCommand = new RelayCommand<Son>(
+                        (param) =>
+                        {
+                            var par = param as Son;
+                            IsAdded = !IsAdded;
+                            if (isAdded == true)
+                            {
+                              
+                            }
+
+                            else
+                            {
+                              
+                            }
+                            BtnText = "已添加o";
+                            // ElementTheme = param;
+                            //  await ThemeSelectorService.SetThemeAsync(param);
+                        });
+                }
+
+                return _switchThemeCommand;
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string pk { get; set; }
+        public string title { get; set; }
+        public string stitle { get; set; }
+        public string pics { get; set; }
+        public string is_end { get; set; }
+        public string list_icon { get; set; }
+        public string list_stitle { get; set; }
+        public string father_id { get; set; }
+        public string p_pk { get; set; }
+        public ObservableCollection<Son> sons { get; set; }
+        public string skey { get; set; }
+        public string token { get; set; }
+        public string api_url { get; set; }
+        public string block_bg_color { get; set; }
+        public string data_type { get; set; }
+        public string require_web { get; set; }
+        public string pic { get; set; }
+
+        private string _btnText;
+        public string BtnText
+        {
+            get { return IsAdded == true ? "已订阅" : "订阅"; }
+            set
+            {
+                if (_btnText != value)
+                {
+                    _btnText = value;
+                    NotifyPropertyChanged("BtnText");
+                }
+            }
+        }
+
+        private bool isAdded = false;
+        public bool IsAdded
+        {
+            get { return isAdded; }
+
+            set
+            {
+                if (isAdded != value)
+                {
+                    isAdded = value;
+                    NotifyPropertyChanged("IsAdded");
+                }
+            }
+
+        }
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
