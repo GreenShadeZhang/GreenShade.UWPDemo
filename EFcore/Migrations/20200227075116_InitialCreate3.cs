106280eb6c062abbdfb3e2eed22b765917a9327c
gil <gil.zhang.dev@outlook.com>
2020-02-27 16:17:19 +0800

    ef core 3.1的测试

---
+++ EFcore/Migrations/20200227075116_InitialCreate3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFcore.Migrations
+{
+    public partial class InitialCreate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestUrl3",
+                table: "Blogs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestUrl3",
+                table: "Blogs");
+        }
+    }
+}
---
+++ GreenShade.UWP.UseEFCore3.1/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using EFcore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Toolkit.Uwp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace GreenShade.UWP.UseEFCore3._1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var dbFile = await ApplicationData.Current.LocalFolder.TryGetItemAsync("blogging.db") as StorageFile;
+            if (null == dbFile || SystemInformation.IsFirstRun)
+            {
+                // first time ... copy the .db file from assets to local  folder
+                var localFolder = ApplicationData.Current.LocalFolder;
+                var originalDbFileUri = new Uri("ms-appx:///Assets/blogging.db");
+                var originalDbFile = await StorageFile.GetFileFromApplicationUriAsync(originalDbFileUri);
+                if (null != originalDbFile)
+                {
+
+                    dbFile = await originalDbFile.CopyAsync(localFolder, "blogging.db", NameCollisionOption.ReplaceExisting);
+
+                }
+            }
+            try
+            {
+                using (var db = new BloggingContext())
+                {
+                    //这里是传绝对路径给的数据上下文
+                   db.DbFilePath = dbFile.Path;
+                    db.Database.Migrate();
+                    //// Create
+                    db.Add(new Blog { Url = "http://blogs.msdn.com/adonet", TestUrl = "http://blogs.msdn.com/adonet" });
+                    db.SaveChanges();
+                    // Read
+                    var blog = db.Blogs
+                        .OrderBy(b => b.BlogId)
+                        .ToList().Select(b => b.TestUrl);
+                    SQLite.ItemsSource = blog;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
